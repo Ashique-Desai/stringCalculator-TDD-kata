a3ed490551a41f1637991836d32469e3ba9b9a3e
Ashique-Desai <desai.ashique@gmail.com>
2025-01-17 11:56:19 +0530

    handle a different delimiter

--- add.js
+++ add.js
@@ -1,5 +1,11 @@
 const add = (inputString) => {
-    const processNewLineAndComma = inputString.split(/,|\n/).map(Number)
+    console.log({ inputString })
+    if (inputString.startsWith('//')) {
+        console.log('Yes starts with //')
+        inputString = inputString.slice(2)
+    }
+    const processNewLineAndComma = inputString.split(/,|\n|;/).map(Number)
+    console.log({ processNewLineAndComma })
     const add = processNewLineAndComma.reduce((a, b) => a + b, 0);
     return add
 };
