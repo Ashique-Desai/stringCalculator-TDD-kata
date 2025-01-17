ca477285810bf11fb00b5936f309da1d3b9ad4ee
Ashique-Desai <desai.ashique@gmail.com>
2025-01-17 10:36:33 +0530

    add test for 2 number string, shall return sum

--- __tests__/add.js
+++ __tests__/add.js
@@ -7,3 +7,7 @@ test('should return 0 for an empty string', () => {
 test('should return the number for a single number string', () => {
     expect(add('5')).toBe(5);
 });
+
+test('should return the sum of 2 number strings separated by a comma', () => {
+    expect(add('1,2')).toBe(3);
+})
