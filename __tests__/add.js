91d024c4b5785ed6f89d2574633dc57635d3c2aa
Ashique-Desai <desai.ashique@gmail.com>
2025-01-17 10:26:33 +0530

    add test for single number string to return number

--- __tests__/add.js
+++ __tests__/add.js
@@ -3,3 +3,7 @@ const add = require('../add');
 test('should return 0 for an empty string', () => {
     expect(add('')).toBe(0);
 })
+
+test('should return the number for a single number string', () => {
+    expect(add('5')).toBe(5);
+});
