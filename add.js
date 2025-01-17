3869f3eadd7ba3e1138d3fa9ae9996d31eed58b1
Ashique-Desai <desai.ashique@gmail.com>
2025-01-17 10:44:01 +0530

    refactor code after passing tests

--- add.js
+++ add.js
@@ -1,16 +1,7 @@
 const add = (inputString) => {
-    if (inputString === "") {
-        return 0;
-    }
-    if (inputString.length === 1) {
-        return Number(inputString)
-    }
-    if (inputString.length >= 2) {
-        const numbers = inputString.split(',').map(Number)
-        const add = numbers.reduce((a, b) => a + b, 0);
-        console.log({ numbers })
-        return add
-    }
+    const numbers = inputString.split(',').map(Number)
+    const add = numbers.reduce((a, b) => a + b, 0);
+    return add
 };
 
 module.exports = add;
