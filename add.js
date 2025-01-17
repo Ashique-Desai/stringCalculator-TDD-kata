16221c943826d3f6b4584746caa6ca0b8736e63e
Ashique-Desai <desai.ashique@gmail.com>
2025-01-17 10:39:08 +0530

    handle 2 number string to return sum of numbers

--- add.js
+++ add.js
@@ -5,6 +5,12 @@ const add = (inputString) => {
     if (inputString.length === 1) {
         return Number(inputString)
     }
+    if (inputString.length >= 2) {
+        const numbers = inputString.split(',').map(Number)
+        const add = numbers.reduce((a, b) => a + b, 0);
+        console.log({ numbers })
+        return add
+    }
 };
 
 module.exports = add;
