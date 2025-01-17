de3a575bc83a1703f2219aff9619eba7caa76250
Ashique-Desai <desai.ashique@gmail.com>
2025-01-17 12:29:08 +0530

    handle negative numbers and throw error

--- add.js
+++ add.js
@@ -1,10 +1,18 @@
 const add = (inputString) => {
-    // check for custom delimiter at start
+    // check for custom delimiter // at start and remove it
     if (inputString.startsWith('//')) {
         inputString = inputString.slice(2);
     }
     // split the string by commas, newLines or semicolons and convert each part to a number
     const numbers = inputString.split(/,|\n|;/).map(Number);
+
+    const negativeNumbers = numbers.filter((number) => number < 0)
+
+    // do not allow negative numbers
+    if (negativeNumbers.length > 0) {
+        throw new Error(`negative numbers not allowed ${negativeNumbers.join(', ')}`);
+    }
+
     const sum = numbers.reduce((a, b) => a + b, 0);
     return sum
 };
