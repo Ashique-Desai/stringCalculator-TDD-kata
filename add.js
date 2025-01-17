e0af5f56e85d4919e256504944bd7f13f8c6f0ec
Ashique-Desai <desai.ashique@gmail.com>
2025-01-17 10:29:14 +0530

    handle single number string to return the number

--- add.js
+++ add.js
@@ -2,6 +2,9 @@ const add = (inputString) => {
     if (inputString === "") {
         return 0;
     }
+    if (inputString.length === 1) {
+        return Number(inputString)
+    }
 };
 
 module.exports = add;
