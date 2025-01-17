69ff63ab6a2500bf4d6ee908381abc9532201d51
Ashique-Desai <desai.ashique@gmail.com>
2025-01-17 11:17:45 +0530

    handle new line between numbers instead of comma

--- add.js
+++ add.js
@@ -1,6 +1,6 @@
 const add = (inputString) => {
-    const numbers = inputString.split(',').map(Number);
-    const add = numbers.reduce((a, b) => a + b, 0);
+    const processNewLineAndComma = inputString.split(/,|\n/).map(Number)
+    const add = processNewLineAndComma.reduce((a, b) => a + b, 0);
     return add
 };
 
